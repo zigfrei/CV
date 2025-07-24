d282ce513c3a3f3dd5887d6c48514a30b605631a
zigfrei <zigfrei.zf@gmail.com>
2025-07-25 00:04:56 +0300

    add schema 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { definePerson } from 'nuxt-schema-org/schema';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
 
   modules: [
+    'nuxt-schema-org',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@pinia/nuxt',
     '@nuxtjs/seo',
-    'nuxt-schema-org',
     '@nuxtjs/sitemap',
   ],
   css: ['~/assets/styles/main.css'],
@@ -23,37 +25,34 @@ export default defineNuxtConfig({
     ],
   },
   schemaOrg: {
-    enabled: true,
-    identity: {
-      type: 'Person',
+    identity: definePerson({
+      // Basic Information, if applicable
       name: 'Ревазашвили Георгий',
-      legalName: 'Ревазашвили Георгий',
+      givenName: 'Георгий',
+      familyName: 'Ревазашвили',
+      additionalName: 'Владимирович', // middle name or other additional names
+      alternateName: 'Георгий Д. Ревазашвили',
+
+      // Profile Information, if applicable
+      image: '/avatar-photo.jpg',
+      description:
+        'Фронтенд разработчик с опытом работы в веб-разработке и мобильной разработке. Специализируюсь на создании адаптивных и доступных веб-приложений с использованием современных технологий и инструментов.',
       jobTitle: 'Фронтенд разработчик',
-      // url: 'https://github.com/zigfrei',
+
+      // Contact & Social, if applicable
+      email: 'georgyrevazashvili@yandex.ru',
+      url: 'https://revaz.vercel.app/',
       sameAs: [
         'https://www.linkedin.com/in/zigfrei',
         'https://github.com/zigfrei',
       ],
-      // image: 'https://github.com/zigfrei/images/avatar.png',
-      description:
-        'Фронтенд разработчик с опытом работы в веб-разработке и мобильной разработке. Специализируюсь на создании адаптивных и доступных веб-приложений с использованием современных технологий и инструментов.',
-      email: 'georgyrevazashvili@yandex.ru',
-      knowsLanguage: [
-        {
-          '@type': 'Language',
-          name: 'Русский',
-          alternateName: 'Russian',
-          proficiencyLevel: 'Native',
-        },
-        {
-          '@type': 'Language',
-          name: 'Английский',
-          alternateName: 'English',
-          proficiencyLevel: 'Professional working proficiency',
-        },
-      ],
-    },
-    defaults: false,
-    reactive: true,
+
+      // Professional Details, if applicable
+      worksFor: {
+        '@type': 'Organization',
+        name: 'Switch Case',
+        url: 'https://switchcase.dev',
+      },
+    }),
   },
 });
