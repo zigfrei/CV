b1d97287b6427076611accbd0d11fa617ea86efd
zigfrei <zigfrei.zf@gmail.com>
2025-07-31 20:55:45 +0300

    change head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,21 @@ import { definePerson } from 'nuxt-schema-org/schema';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/png", sizes: "120x120", href: "/favicon-120x120.png" },
+        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" }, 
+      ],
+        meta: [
+    { name: "yandex-verification", content: "fca727eaa2be41f5" },
+  ]
+    },
+  },
   modules: [
     'nuxt-schema-org',
     '@nuxt/eslint',
